3e388e6b1f11730088815d03e749d49e6de696db
jliip51 <jliip51@gmail.com>
2017-08-08 00:14:31 -0400

    added more data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Friend Finder
 
 ## Description
-Friend Finder takes data from a user survey to determine the most compatible match as a friend. The user answers 10 questions with a 1 - 5 scale and Friend Finder analyzes the data against all other users responses. The user receives a recommended match with the closest score.
+Friend Finder takes data from a user survey to determine the most compatible match as a friend. The user answers 10 questions with a 1 - 5 scale and Friend Finder analyzes their scores against all other users responses. The user receives a recommended match with the closest score.
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,48 +1,48 @@
 var friends = [
   {
     name: "Minnie",
-    photo_url: "Female",
+    photo_url: "https://www.disneyclips.com/imagesnewb/images/0193.gif",
     scores: [4, 5, 2, 1, 3, 5, 5, 1, 4, 5]
   },
   {
     name: "Mickey",
-    photo_url: "Male",
+    photo_url: "https://www.disneyclips.com/imagesnewb/images/clipmickeycool.gif",
     scores: [1, 4, 3, 4, 5, 1, 2, 4, 5, 5]
   },
   {
     name: "Donald",
-    photo_url: "Male",
+    photo_url: "https://www.disneyclips.com/imagesnewb/images/clipdon6.gif",
     scores: [5, 2, 1, 1, 3, 4, 2, 5, 1, 2]
   },
   {
     name: "Daisy",
-    photo_url: "Female",
-    scores: [2, 3, 3, 4, 1, 5, 5, 5, 2, 1]
+    photo_url: "https://www.disneyclips.com/imagesnewb/images/daisy12.png",
+    scores: [2, 3, 3, 4, 1, 1, 1, 3, 5, 5]
   },
   {
     name: "Goofy",
-    photo_url: "Male",
+    photo_url: "https://www.disneyclips.com/imagesnewb/images/goofy-face2.png",
     scores: [4, 5, 5, 3, 1, 2, 3, 4, 4, 4]
   },
   {
-    name: "Sally",
-    photo_url: "Female",
+    name: "Luke",
+    photo_url: "https://s-media-cache-ak0.pinimg.com/originals/e9/a7/16/e9a716a8d14b67bbdcdeb9334c76b0cd.jpg",
     scores: [5, 3, 3, 4, 5, 1, 1, 2, 2, 2]
   },
   {
-    name: "Harry",
-    photo_url: "Male",
+    name: "Leia",
+    photo_url: "https://laughingsquid.com/wp-content/uploads/carrie_fisher_princess_leia_by_dave_daring-d5laxr7.jpg",
     scores: [5, 3, 3, 4, 5, 1, 1, 2, 2, 2]
   },
   {
     name: "Britney",
-    photo_url: "Female",
+    photo_url: "http://img.usmagazine.com/article-leads-vertical-300/1250531162_britney_spears_290x402.jpg",
     scores: [5, 5, 3, 5, 2, 4, 3, 4, 5, 3]
   },
   {
-    name: "Ringo",
-    photo_url: "Male",
-    scores: [2, 2, 4, 4, 4, 5, 2, 3, 4, 5]
+    name: "Justin",
+    photo_url: "https://yt3.ggpht.com/-lIauTqVpm7c/AAAAAAAAAAI/AAAAAAAAAAA/4Cq1U_gDtO8/s900-c-k-no-mo-rj-c0xffffff/photo.jpg",
+    scores: [2, 2, 4, 4, 4, 5, 2, 3, 5, 5]
   },
 ];
 
